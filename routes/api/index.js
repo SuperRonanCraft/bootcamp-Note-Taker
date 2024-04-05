2f09800b68044822dc6e46df0ab17b474ce26cf8
RonanCraft <SuperRonanCraft@gmail.com>
2024-04-04 23:00:15 -0400

    final comments

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,6 +2,8 @@ const route = require("express").Router();
 
 const notesRouter = require("./notes");
 
+//Index route for anything in /api
+
 // api/notes route
 route.use("/notes", notesRouter);
 
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,11 @@ const router = require("express").Router();
 const api = require("./api/index");
 
 //html routes
+//Handle any other base `/` routes
 router.use(require("./htmlRoutes"));
 
 // /api routes
+//Handle all /api routing
 router.use("/api", api);
 
 module.exports = router;
